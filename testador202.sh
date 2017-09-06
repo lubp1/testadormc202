1a9301ea14d07d3dc01ada7a9430baec055d9399
Lucas Bertoloto <lucasbertoloto@outlook.com>
2017-09-06 21:37:07 +0000

    Teste de compilação

--- testador202.sh
+++ testador202.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-echo "Hello World"
+read -p "Insira o número do lab: " numero
+
+$(gcc lab$numero.c -o lab$numero)
+
+if [ $? -ne 0 ]
+	then
+	echo "Erro na compilação"
+	exit
+fi
+
+echo "Compilação OK"
+echo ""
