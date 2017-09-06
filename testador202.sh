bc6a4ba97f14d505fafa22d7052f07443f0f0e63
Lucas Bertoloto <lucasbertoloto@outlook.com>
2017-09-06 21:58:55 +0000

    Agora ele compara testes!

--- testador202.sh
+++ testador202.sh
@@ -9,5 +9,23 @@ if [ $? -ne 0 ]
 	exit
 fi
 
-echo "Compilação OK"
+echo "Sem erros de compilação"
 echo ""
+
+for file in testes/*.in
+do
+	echo Teste $file
+	file="${file%.*}"
+	./lab$numero < $file.in > $file.res
+	$(diff $file.out $file.res > diff.txt)
+
+	if [ $? -eq 0 ]
+	then echo "teste $file OK"
+	else
+	echo "erro no teste $file"
+	cat diff.txt
+	fi
+
+	echo ""
+
+done
